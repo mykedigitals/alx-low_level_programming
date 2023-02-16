c137fd1e827fcd2b64288309789a01cc4a394fa0
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-02-16 23:39:56 +0100

    git

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Description: this program prints using printf stdio
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
-printf("with proper grammar, but the outcome is a piece of art, \n");
-return (0);
+{ printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
