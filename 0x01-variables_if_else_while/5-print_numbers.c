5427a597e5b248924e17983eb2c4882c591ff569
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-02-18 13:03:52 +0100

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- declaring var,then printing digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int numb;
+		for (numb = 0; numb <= 9; numb++)
+			{ printf("%d", numb);
+				}
+
+		printf("\n");
+		return (0);
+
+}
