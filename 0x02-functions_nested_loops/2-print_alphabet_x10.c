f5e4834229a8746a93f1d84d69baced036802779
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-02-21 16:57:00 +0100

    2-print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Print alphabet 10times, in lowercase
+ * followed by a new line
+ * Return: always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int p = 0;
+	char x;
+	
+	while (p++ < 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
