614c0900566958859c9b89d9aed7573e3bb25dbc
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-02-23 12:54:45 +0100

    uppercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing prototypes for
+ * all functions used in more functions_nested_loops directory.
+ *
+ */
+
+int _putchar (char c);
+int _isupper(int c);
+
+#endif
