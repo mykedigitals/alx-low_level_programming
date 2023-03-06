c8907eebd062d8e284aec0ce8429593d2ff21206
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-03-06 16:28:52 +0100

    set string using pointer of a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - This sets the value of a pointer to a character.
+ * @s: Reps the pointer.
+ * @to: The character.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
