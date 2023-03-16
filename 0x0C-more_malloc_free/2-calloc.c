d7e537b64a3fc383277f8b7ef7dada29303e008f
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-03-16 15:44:35 +0100

    fills memory with a constant byte

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -34,10 +34,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL)
+		return (NULL);
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
-	_memset(ptr, 0, nmemba * size);
+	_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
