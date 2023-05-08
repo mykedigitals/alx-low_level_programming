2faf193ea1f73f4a3e1352998f192befd4fce4dc
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-05-08 16:07:14 +0100

    a function that reads a text file and prints it

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
+
